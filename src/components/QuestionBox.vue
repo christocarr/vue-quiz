0377c8bdc5052c13610a05babb6292f5a13f9056
Chris Carr <christocarr@outlook.com>
2020-04-18 12:14:24 +0100

    Add answers to jumbotron

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -5,8 +5,8 @@
         {{ currentQuestion.question }}
       </template>
       <hr>
-      <p>
-        List of answers
+      <p v-for="(answer, index) in answers" :key="index">
+        {{ answer }}
       </p>
       <b-button variant="primary">Submit</b-button>
       <b-button @click="nextQuestion" variant="primary">Next</b-button>
@@ -19,6 +19,13 @@ export default {
   props: {
     currentQuestion: Object,
     nextQuestion: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
   }
 }
 </script>
